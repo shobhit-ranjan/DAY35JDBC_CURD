08fc0a1209b7bf861e9551099c6faf483ea137ef
shobhit-ranjan <shobhit.ranjan03@gmail.com>
2021-09-02 20:14:16 +0530

    UC4

--- jdbc_DayThirtyFive.java
+++ jdbc_DayThirtyFive.java
@@ -2,12 +2,15 @@ import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 public class jdbc_DayThirtyFive {
 
 	public static void main(String[] args) {
 		getsqlConnection();
-		writeempData();
+		//writeempData();
+		readEmployeePayroll();
 
 	}
 	
@@ -40,7 +43,40 @@ public class jdbc_DayThirtyFive {
 			}
 		}
 	}
-	
+	private static void readEmployeePayroll() {
+		System.out.println("Displaying all data of employee_payroll table");
+		Connection conn = getsqlConnection();
+
+		try {
+			if (conn != null) {
+				String readEmpPayroll = "SELECT * FROM employee_payrolldar35";
+
+				Statement statement = conn.createStatement();
+				ResultSet resultSet = statement.executeQuery(readEmpPayroll);
+				while (resultSet.next()) {
+					Integer id = resultSet.getInt(1);
+					String name = resultSet.getString(2);
+					Integer salary = resultSet.getInt(3);
+					String date = resultSet.getString(4);
+					String row = String.format("User record: \n Id: %d, \n Name: %s,\n DAte: %d, \n", id, name,
+							salary, date);
+					System.out.println(row);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (conn != null) {
+				try {
+					conn.close();
+				} catch (SQLException sqlException) {
+					System.out.println(sqlException.getMessage());
+
+				}
+			}
+		}
+
+	}
 	private static Connection getsqlConnection() {
 		Connection conn = null;
 		String hostUrl = "jdbc:mysql://localhost:3306/emp_payroll";
